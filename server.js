ff413693853b271b007de04c3a34f477c40e822a
Bryan Yunis <bryany@booj.com>
2021-02-23 02:17:00 -0500

    Add axios, cors; minor changes to response objects

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const {generateSha} = require('./utils/generateId.js')
 require('dotenv').config()
 const {get, set} = require('./db/index.js')
@@ -10,15 +11,24 @@ const {get, set} = require('./db/index.js')
 app.use(helmet())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.send('API HEALTHY')
 })
 app.get('/hash-lookup/:hash', async (req, res) => {
 	const {hash} = req.params
-	console.log('has', hash)
 	const dbResponse = await get(hash)
-	console.log('dbres', dbResponse)
+	if (dbResponse) {
+		return res.json({dbResponse})
+	} else {
+		res.status(500).json({message: 'Not found'})
+	}
+})
+
+app.get('/hash-redirect/:hash', async (req, res) => {
+	const {hash} = req.params
+	const dbResponse = await get(hash)
 	if (dbResponse) {
 		return res.redirect(`http://${dbResponse}`)
 	} else {
@@ -36,6 +46,19 @@ app.post('/add', async (req, res) => {
 		const dbResponse = await set(sha, url)
 		if (dbResponse === 'OK') {
 			res.json({alreadyExists: false, hash: sha, url})
+		} else res.status(500).json({error: 'Error saving your hash'})
+	}
+})
+
+app.post('/add/custom', async (req, res) => {
+	const { slug, url } = req.body
+	const dbResponse = await get(slug)
+	if (dbResponse) {
+		res.json({alreadyExists: true, slug, url})
+	} else {
+		const dbResponse = await set(slug, url)
+		if (dbResponse === 'OK') {
+			res.json({alreadyExists: false, slug, url})
 		} else res.status(500).json({error: 'Error saving your hash'})
 	}
 })
--- utils/generateId.js
+++ utils/generateId.js
@@ -2,7 +2,7 @@ const SHA1 = require("crypto-js/sha1");
 
 const generateSha = (message) => {
     const sha = SHA1(message).toString()
-    return sha.slice(0, 5)
+    return sha.slice(0, 4)
 }
 
 module.exports = {generateSha}
