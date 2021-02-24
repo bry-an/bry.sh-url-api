5d59ee6ddb30c93f19f3dd561c1375a2fb4ad826
Bryan Yunis <bryany@booj.com>
2021-02-23 21:57:47 -0500

    serve frontend statically at /, redirect at /:hash

--- server.js
+++ server.js
@@ -7,14 +7,27 @@ const cors = require('cors')
 const {generateSha} = require('./utils/generateId.js')
 require('dotenv').config()
 const {get, set} = require('./db/index.js')
+const path = require('path')
 
 app.use(helmet())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-	res.send('API HEALTHY')
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/:slug', async (req, res) => {
+	const {slug} = req.params
+	console.log('slug', slug)
+	const fullUrl = await get(slug)
+	if (fullUrl) {
+		console.log('FOUND', fullUrl)
+		res.redirect(fullUrl)
+	} else {
+		console.log('FOUND NOTHING')
+		res.redirect('../')
+	}
 })
 app.get('/hash-lookup/:hash', async (req, res) => {
 	const {hash} = req.params
