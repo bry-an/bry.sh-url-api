194b4b8e60681feacd8e62030a9038c0c86e17e9
Bryan Yunis <bryany@booj.com>
2021-02-26 20:53:44 -0500

    Dockerize app
    Bring app into /src folder

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 const redis = require('redis')
 
 const client = redis.createClient({
-    host: '0.0.0.0',
+    host: 'redis',
     port: 6379,
 });
 
--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/:slug', async (req, res) => {
 	const {slug} = req.params
